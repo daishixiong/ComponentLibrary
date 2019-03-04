520ece031053ba37f7a5974eb30d6318a5ab665f
DaiSX <sxdai@hillinsight.com>
2019-03-04 14:32:24 +0800

    fix

---
+++ my-menu-item.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <template v-if="(!item.children) || item.children.length === 0">
+            <el-menu-item :index="item.key">
+                <i :class="item.icon"></i>
+                <span slot="title">{{item.name}}</span>
+            </el-menu-item>
+        </template>
+        <el-submenu v-else :index="item.key">
+            <template slot="title">
+                <i :class="item.icon"></i>
+                <span>{{item.name}}</span>
+            </template>
+            <my-menu-item v-for="child in item.children" :key="child.key" :index="child.key" :item="child"></my-menu-item>
+        </el-submenu>
+    </div>
+</template>
+<style lang="less"></style>
+<script>
+export default {
+    name: 'MyMenuItem',
+    props: {
+        item: {
+            type: Array,
+            default() {
+                return [];
+            }
+        }
+    }
+};
+</script>
---
+++ my-menu.vue
@@ -0,0 +1,41 @@
+<template>
+    <el-menu
+        :default-active="defaultActive"
+        @select="handleClick"
+        class="el-menu-vertical-demo">
+        <my-menu-item v-for="item in menuData" :key="item.key" :item="item"></my-menu-item>
+    </el-menu>
+</template>
+<style lang="less" scoped></style>
+
+<script>
+import MyMenuItem from './my-menu-item.vue';
+export default {
+    components: {
+        MyMenuItem
+    },
+    props: {
+        defaultActive: {
+            type: [String, Number],
+            default() {
+                return '';
+            }
+        },
+        menuData: {
+            type: Array,
+            default() {
+                return [];
+            }
+        }
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        handleClick(index, indexPath) {
+            this.$emit('select', index, indexPath);
+        }
+    }
+};
+</script>
+
